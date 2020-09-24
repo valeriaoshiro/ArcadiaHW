ce3fd8c5281414c8fb33890978140c27ff01f409
Valeria Oshiro <valeriaoshiro19@gmail.com>
2020-09-24 15:52:53 -0700

    Add App.js

---
+++ client/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+    return (
+          <div>
+              <header>
+                Adopt Me!
+              </header>
+          </div>
+    );
+  };
+  
+  render(<App />, document.getElementById("root"));
