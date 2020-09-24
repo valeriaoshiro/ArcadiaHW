1d8377bec66345bc63851a2fc5e1dd6a3f70f565
Valeria Oshiro <valeriaoshiro19@gmail.com>
2020-09-24 16:27:31 -0700

    Add React Router

--- client/App.js
+++ client/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <header>Adopt Me!</header>
+      <header>Contact Management</header>
+      <Router>
+        <Switch></Switch>
+      </Router>
     </div>
   );
 };
