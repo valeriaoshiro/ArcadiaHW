bea1ded1516798a1a158191b2fb717c823f0d05b
Valeria Oshiro <valeriaoshiro19@gmail.com>
2020-09-25 15:42:00 -0700

    Add context and contacts api call

--- client/App.js
+++ client/App.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import AddContact from "./components/AddContact";
 import EditContact from "./components/EditContact";
+import ContactContext from "./context/ContactContext";
 
 const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/contacts")
+      .then((response) => response.json())
+      .then((response) => setContacts(response));
+  }, [contacts]);
+
   return (
     <div>
-      <header>Contact Management</header>
-      <Router>
-        <Switch>
-          <Route path="/add" component={AddContact} />
-          <Route path="/edit/:id" component={EditContact} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
+      <ContactContext.Provider value={contacts}>
+        <header>Contact Management</header>
+        <Router>
+          <Switch>
+            <Route path="/add" component={AddContact} />
+            <Route path="/edit/:id" component={EditContact} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
+      </ContactContext.Provider>
     </div>
   );
 };
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import ContactContext from "./../context/ContactContext";
 
 const Home = () => {
+  const contacts = useContext(ContactContext);
+
   return (
     <div>
       <h1>Home</h1>
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            {contact.name} | {contact.phone} | {contact.email}
+            <Link to={`/edit/${contact.id}`}>Edit</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
