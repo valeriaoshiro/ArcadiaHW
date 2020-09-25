5e2d4ab436abd20c3f0694ec46d0d1df44c19e8d
Valeria Oshiro <valeriaoshiro19@gmail.com>
2020-09-25 14:12:37 -0700

    Add Home, AddContact, EditContact pages

--- client/App.js
+++ client/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import AddContact from "./components/AddContact";
+import EditContact from "./components/EditContact";
 
 const App = () => {
   return (
     <div>
       <header>Contact Management</header>
       <Router>
-        <Switch></Switch>
+        <Switch>
+          <Route path="/add" component={AddContact} />
+          <Route path="/edit/:id" component={EditContact} />
+          <Route path="/" component={Home} />
+        </Switch>
       </Router>
     </div>
   );
